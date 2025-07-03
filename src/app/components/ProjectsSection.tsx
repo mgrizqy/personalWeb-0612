06832a48114e9572a3d4c82de5fba86f6e05456c
mgrizqy <mgrizqy.work@gmail.com>
2025-07-04 06:15:53 +0700

    personalWeb_04_projectsection_star_addition_gridcol_tweak

--- src/app/components/ProjectsSection.tsx
+++ src/app/components/ProjectsSection.tsx
@@ -69,7 +69,7 @@ export default function ProjectsSection() {
                 <h2 className="text-5xl font-bold mt-2 mb-16">Featured Projects</h2>
             </div>
             
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
                 {projects.map((project,index) => (
                    
                     <ProjectCard key={project.id} id={project.id} project={project} onOpenModal={() => setSelectedProject(project)} onClose={() => setSelectedProject(null)} selectedProject={selectedProject}  />
